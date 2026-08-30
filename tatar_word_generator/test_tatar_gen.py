68bc6808b68a2c54ddb8bfc5e21e793b63a3580e
Trid147 <your_email@example.com>
2026-08-30 19:33:14 +0300

    Add test for nouns and verbs

---
+++ tatar_word_generator/test_tatar_gen.py
@@ -0,0 +1,26 @@
+from tatar_gen import Noun, Verb
+
+#noun tests
+
+def test_noun_plural_hard_vowel():
+    noun = Noun('китап')
+    assert noun.to_plural() == 'китаплар'
+
+def test_noun_plural_nasal():
+    noun = Noun('урман')
+    assert noun.to_plural() == 'урманнар'
+
+def test_noun_cases():
+    noun = Noun('китап')
+    assert noun.add_case('of') == 'китапның'
+    assert noun.add_case('in') == 'китапта'
+
+# verb tests
+
+def test_verb_stem_extraction():
+    verb = Verb("язарга")
+    assert verb.stem == "яз"
+
+def test_verb_past_tense():
+    verb = Verb("язарга")
+    assert verb.add_tense("past", 1, is_plural=False) == "яздым"
